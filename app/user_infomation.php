71a6153d7653a35b6f75cff4efa98c43720eb05f
つがり <connect@tsugareco.net>
2019-02-27 20:59:16 +0900

    Add files via upload

---
+++ app/user_infomation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class user_infomation extends Model
+{
+    public $incrementing = false;
+    protected $primaryKey = 'user_id';
+}
