660794996578f850997ac1e5cb0a7f36efca63b4
つがり <45946481+tsugarii@users.noreply.github.com>
2019-02-27 17:12:01 +0900

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,39 @@
 <?php
 
-use App\Http\Middleware\fivechMiddleware;
+use App\Http\Middleware\FiveChMiddleware;
+use App\Http\Middleware\ResponseMiddleware;
 
+//ログインとか
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+//トップページ
 Route::get('/', function () {
     return view('main');
 });
+
+//ユーザーページとセッティングページ
+Route::get('user/{user_id}', 'UserController@user_page');
+Route::get('settings', 'UserController@settings');
+
+Route::post('settings/update', 'UserController@settings_update');
 
 //5ch互換部分
 Route::get('test/bbs.cgi', 'FiveChController@write_ready');
 Route::post('test/bbs.cgi', 'FiveChController@write')
 ->middleware(FiveChMiddleware::class);
 
-//JSONを吐く
+Route::get('/min', 'FiveChController@index');
+
+//JSON
+
+//板のデータをJSONで返す
 Route::get('json/{board_key}', 'JsonController@board_read');
 Route::get('json/{board_key}/hot/', 'JsonController@board_read');
 Route::get('json/{board_key}/new/', 'JsonController@board_read_new');
 Route::get('json/{board_key}/top/', 'JsonController@board_read_top');
 
+//スレッドののデータをJSONで返す
 Route::get('json/{board_key}/{thread_key}', 'JsonController@thread_read');
 Route::get('json/{board_key}/{thread_key}/{res_select}', 'JsonController@res_read');
 
@@ -26,14 +43,23 @@ Route::get('/test/read.cgi/{board_key}/{thread_key}/', 'FiveChController@read_cg
 Route::get('/test/read.cgi/{board_key}/{thread_key}/{res_number}', 'FiveChController@read_cgi_res');
 
 //ツガレコミン独自部分
+
+//板を開いた場合
 Route::get('r/{board_key}', 'TsugarecominController@board_read');
 Route::get('r/{board_key}/hot/', 'TsugarecominController@board_read');
 Route::get('r/{board_key}/new/', 'TsugarecominController@board_read_new');
 Route::get('r/{board_key}/top/', 'TsugarecominController@board_read_top');
 
+//Upvoteをする場合
+Route::post('upvote/{board_key}/{thread_key}', 'TsugarecominController@thread_upvote');
 
-Route::get('r/{board_key}/{thread_key}', 'TsugarecominController@thread_read');
-Route::get('r/{board_key}/{thread_key}/{res_select}', 'TsugarecominController@res_read');
+//スレッドを開いた場合
+Route::get('r/{board_key}/{thread_key}', 'TsugarecominController@thread_read')
+->middleware(ResponseMiddleware::class);
+Route::get('r/{board_key}/{thread_key}/{res_select}', 'TsugarecominController@res_read')
+->middleware(ResponseMiddleware::class);
 
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('r/{board_key}/{thread_key}/id/{id}', 'TsugarecominController@thread_id_read');
+
+//いいねをした場合
+Route::post('like/{board_key}/{thread_key}/{comment_key}', 'TsugarecominController@res_like');
